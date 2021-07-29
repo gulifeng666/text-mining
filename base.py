d08ba2b7a6632eea4723737ec29d4087e23521b5
gulifeng666 <gulifeng666@163.com>
2021-07-29 15:49:05 +0800

    test

--- base.py
+++ base.py
@@ -2,14 +2,14 @@ import re
 import jieba
 from gensim.corpora import Dictionary
 class Base:
-    text_path = ''
-    corpus = ''
+    text_corpus_dict = {}
+    text_dictionary_dict = {}
+    text_dict = {}
     @classmethod
     def build_dictionary(cls,text_path,use_stop_word):
-        cls.text_path = text_path
-        cls.text = cls.preprocess_text(text_path,use_stop_word)
-        cls.dic = Dictionary(cls.text)
-        cls.corpus = [cls.dic.doc2bow( doc) for doc in cls.text]
+        cls.text_dict[text_path]= cls.preprocess_text(text_path,use_stop_word)
+        cls.text_dictionary_dict[text_path] = Dictionary(cls.text_dict[text_path])
+        cls.text_corpus_dict[text_path] = [ cls.text_dictionary_dict[text_path].doc2bow( doc) for doc in cls.text_dict[text_path]]
     @classmethod
     def preprocess_text(cls,text_path,use_stop_word):
         with open(text_path,mode='r',encoding='utf-8') as f:
--- keyword_extractor/tfidf.py
+++ keyword_extractor/tfidf.py
@@ -1,16 +1,20 @@
 from gensim.models import TfidfModel
 from base import Base
 import jieba
-#
+import itertools
 class TFIDF(Base):
     def __init__(self,file_path,use_stop_word = True):
-
-        if (file_path != super().__getattribute__('text_path') or super().__getattribute__('corpus') == None):
+        self.file_path= file_path
+        if (super().__getattribute__('text_corpus_dict').get(file_path,0)  == 0):
              super().build_dictionary(file_path,use_stop_word)
         self.build_model()
     def extract_keyword(self,number = 100):
-        tfidfvalue = [self.model[tokens] for tokens in super().__getattribute__('corpus')]
-      
+        self.tfidfvalue = [self.model[tokens] for tokens in super().__getattribute__('text_corpus_dict').get(self.file_path)]
+        tfidf_sorted = sorted(itertools.chain(*self.tfidfvalue),key = lambda x:x[1],reverse = 1)
+        number = number if number<=len(tfidf_sorted) else len(tfidf_sorted)
+        tmp_dict = super().__getattribute__('text_dictionary_dict').get(self.file_path)
+        key_words = [ tmp_dict[item[0]] for item in  tfidf_sorted ]
+        return key_words[:number]
     def build_model(self):
-        self.model = TfidfModel(super().__getattribute__('corpus'))
+        self.model = TfidfModel(super().__getattribute__('text_corpus_dict').get(self.file_path))
 
