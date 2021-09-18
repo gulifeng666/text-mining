66d744d4af7bd084f7f1cb2c85491b3058a9429d
gulifeng666 <49009159+gulifeng666@users.noreply.github.com>
2021-09-18 10:38:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 
-from keyword_extractor.tfidf import TFIDF
+import jieba.analyse
+
+from keyword_extractor.tfidf import TFIDF,LDA
 
 
 if __name__ == '__main__':
-    model = TFIDF('C:/Users/Administrator/Desktop/test.txt')
-    res = model.extract_keyword()
+   # jieba.analyse.set_idf_path('D:\data\knowledge_corpus\knwldg_inf_1')
+   # res = jieba.analyse.extract_tags(open('D:\data\knowledge_corpus\knwldg_inf_1',encoding='utf-8').read(),topK=100)
+    tfidf_model = TFIDF('D:\data\knowledge_corpus\knwldg_inf_1',use_stop_word=True,stop_save_file='data\cn_stopwords.txt')
+    tfidf_model.extract_keyword('out\out1_tfidf.text', number=100)
+    lda_model = LDA('D:\data\knowledge_corpus\knwldg_inf_1',use_stop_word=True,stop_save_file='data\cn_stopwords.txt')
+    lda_model.extract_keyword('out\out1_lda.text',number=100)
+    lda_model.compare_for_debug(lda_model,'out\debug1_tfidf_lda.text')
+
+
+
+
+
 
 
 
